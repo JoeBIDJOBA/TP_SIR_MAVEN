223e615cd6d707b9ef0fc50145c5a20bdb67e218
JoeBIDJOBA <tibo.les.bob@hotmail.fr>
2015-01-13 15:23:04 +0100

    ajout du projet

---
+++ src/main/java/fr/ISTIC/SIR/TP_MAVEN/App.java
@@ -0,0 +1,25 @@
+package fr.ISTIC.SIR.TP_MAVEN;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+	public int varinutile;
+	
+	
+	/**
+	 * Fonction main
+	 * @param args
+	 */
+    public static void main(String[] args) {
+    	
+        System.out.println("Hello World !");
+        fonctionInutile();
+    }
+    
+    public static void fonctionInutile() {
+    	
+    }
+}
